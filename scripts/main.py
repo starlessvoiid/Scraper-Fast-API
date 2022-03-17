ce97c96c64bbc30014001284ed7210012171b2a2
Adnane Slimani <60299177+Pstar-Adnane@users.noreply.github.com>
2022-03-17 19:29:53 +0100

    modifications

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from scraper import Scraper
+import json
+import csv
+from pprint import pprint
 
 app = FastAPI() # Launching the app
 scraper = Scraper() # Creating instance of Scraper object
@@ -8,3 +11,14 @@ scraper = Scraper() # Creating instance of Scraper object
 async def read_data(tag) :
 
     return scraper.scrapemovies(tag) # Return scraped list of movie datas
+@app.get("/{tag}/download")
+async def read_data(tag) :
+
+    data = scraper.scrapemovies(tag)
+    json_data = json.dumps(data)
+
+    return json_data
+
+    with open(f"data_{tag}",'wb') as file:
+        output_csv = csv.writer(file)
+        output_csv.writerows(json_data)
