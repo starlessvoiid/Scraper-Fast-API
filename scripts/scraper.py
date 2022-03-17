e070b9d2efbbce0749c4586251ef973131c5d081
Adnane Slimani <60299177+Pstar-Adnane@users.noreply.github.com>
2022-03-17 16:38:08 +0100

    Finishing the scraper

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -6,26 +6,37 @@ from random import randint
 #Scraper class
 class Scraper() :
 
-    movie_name = []
-    year = []
-    rating = []
-    genre = []
     
-    pages = [page for page in range(1, 1000, 50)]
 
     def scrapemovies(self, tag) :
+
+        # Return variables
+        movies_data = []
+
+        pages = [1]
+
+        # Looping through all pages
         for page in pages :
-            url = f"https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres={tag}&start={page}&ref_=adv_nxt"
-        s = HTMLSession()
-        r = s.get(url)
-        print(r.status_code)
+            url = f"https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres={tag}&start={str(page)}&ref_=adv_nxt"
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, "html.parser")
+            movie_data = soup.findAll("div", attrs = {"class" : "lister-item mode-advanced"}) # Find movie section
+            sleep(randint(2, 8)) # To avoid crashing
 
-        movies_list = []
+            for item in movie_data : # Loop through each item
 
-        movies = r.html.find("div.lister-item-content")
+                item = {
+                    "name" : item.h3.a.text, #Scrape the name of the movie
 
-        for m in movies :
-            print(m.find("p.text-muted", first = False))
+                    "year" : item.h3.find("span", class_ = "lister-item-year text-muted unbold").text.replace("(", "").replace(")", ""), # Scrape the year of release
 
-movies = Scraper()
-movies.scrapedata("Action")
+                    "rating" : item.find("div", class_ = "inline-block ratings-imdb-rating").text.strip(), # Scrape the rating of movies
+
+                    "genre" : item.p.find("span", class_ = "genre").text.strip(), # Scrape the genre
+
+                    "description" : item.findAll("p", class_ = "text-muted")[1].text.replace("\n", "") # Scrape the description of the movie
+                }
+
+                movies_data.append(item)
+                
+        return movies_data # return a JSON of movie items
