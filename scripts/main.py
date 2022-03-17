624f63906376b749540ce4c0248b48025cf28171
Adnane Slimani <60299177+Pstar-Adnane@users.noreply.github.com>
2022-03-17 20:18:52 +0100

    Finishing the API and starting data preprocessing

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,7 @@ from scraper import Scraper
 import json
 import csv
 from pprint import pprint
+import pandas as pd
 
 app = FastAPI() # Launching the app
 scraper = Scraper() # Creating instance of Scraper object
@@ -15,10 +16,11 @@ async def read_data(tag) :
 async def read_data(tag) :
 
     data = scraper.scrapemovies(tag)
-    json_data = json.dumps(data)
 
-    return json_data
-
-    with open(f"data_{tag}",'wb') as file:
-        output_csv = csv.writer(file)
-        output_csv.writerows(json_data)
+    with open(f"json_data_{tag}.txt", "w") as outfile :
+        json.dump(data, outfile)
+    
+    df = pd.read_json (r'json_data_{tag}.txt')
+    df.to_csv (r'data_csv_{tag}.csv', index = None)
+    
+    return "done"
